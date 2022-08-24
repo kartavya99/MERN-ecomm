8fd51c9f234f9155c2fb94782bd26bec4e243916
kartavya <kartavyas@gmail.com>
2022-08-24 19:36:04 +1000

    Header component added

--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -1,26 +1,56 @@
 import React from "react";
 
-import { Container, Nav, Navbar, Form, NavDropdown } from "react-bootstrap";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { BsCartCheck } from "react-icons/bs";
+import { FaSignInAlt } from "react-icons/fa";
+
+import classes from "./Header.module.css";
 
 const Header = () => {
   return (
-    <Header>
-      <p>Hello</p>
+    <header>
       <Navbar bg="dark" variant="dark" expand="-xl">
         <Container>
-          <Navbar.Brand>E-comm Shop</Navbar.Brand>
+          <Navbar.Brand className={classes.logo}>E-COMM SHOP</Navbar.Brand>
 
-          <NavDropdown>
-            <NavDropdown.Item>Users</NavDropdown.Item>
+          <Nav.Link className={classes.link}>
+            {" "}
+            <span>
+              <BsCartCheck className={classes.cart} />
+            </span>
+            Cart
+          </Nav.Link>
 
-            <NavDropdown.Item>Products</NavDropdown.Item>
+          <NavDropdown title="User" id="user" className={classes.link}>
+            <NavDropdown.Item className={classes.item}>
+              PROFILE
+            </NavDropdown.Item>
+            <NavDropdown.Item className={classes.item}>LOGOUT</NavDropdown.Item>
+          </NavDropdown>
 
-            <NavDropdown.Item>Orders</NavDropdown.Item>
+          <Nav.Link className={classes.link}>
+            <span className={classes["sign-in"]}>
+              <FaSignInAlt />
+            </span>
+            SIGN IN
+          </Nav.Link>
+          <NavDropdown
+            title="Admin"
+            id="adminmenu"
+            className={classes["admin-menu"]}
+          >
+            <NavDropdown.Item className={classes.item}>Users</NavDropdown.Item>
+
+            <NavDropdown.Item className={classes.item}>
+              Products
+            </NavDropdown.Item>
+
+            <NavDropdown.Item className={classes.item}>Orders</NavDropdown.Item>
           </NavDropdown>
         </Container>
       </Navbar>
-    </Header>
+    </header>
   );
 };
 
