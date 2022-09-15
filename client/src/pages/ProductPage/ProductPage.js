a514cc332d5d40d9a0322b5f5a7c97bb9f445d52
kartavya <kartavyas@gmail.com>
2022-09-15 19:33:15 +1000

    singple product page functionality added

--- client/src/pages/ProductPage/ProductPage.js
+++ client/src/pages/ProductPage/ProductPage.js
@@ -22,9 +22,8 @@ const ProductPage = () => {
     variables: { id },
   });
 
-  if (loading) return <Loader />;
-  console.log(data);
-  console.log(data.product.productName);
+  //   console.log(data);
+  //   console.log(data.product.productName);
   return (
     <>
       <Link className="btn btn-light my-3" to="/">
@@ -52,6 +51,106 @@ const ProductPage = () => {
                 <ListGroup.Item>Price: ${data.product.price}</ListGroup.Item>
                 <ListGroup.Item>
                   Description: {data.product.description}
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
+
+            <Col me={3}>
+              <Card>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Price :</Col>
+                      <Col>
+                        <strong>${data.product.price}</strong>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Status:</Col>
+                      <Col>
+                        {data.product.countInStock > 0
+                          ? "In Stock"
+                          : "Out of Stock"}
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+
+                  {data.product.countInStock > 0 && (
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>Qty</Col>
+                        <Col>
+                          <Form.Control
+                            as="select"
+                            value={qty}
+                            onChange={(e) => setQty(e.target.value)}
+                          >
+                            {[...Array(data.product.countInStock).keys()].map(
+                              (x) => (
+                                <option key={x + 1} value={x + 1}>
+                                  {x + 1}
+                                </option>
+                              )
+                            )}
+                          </Form.Control>
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  )}
+
+                  <ListGroup.Item>
+                    <Button
+                      className="btn-block btn btn-dark btn-sm"
+                      type="button"
+                      disabled={data.product.countInStock === 0}
+                    >
+                      ADD TO CART
+                    </Button>
+                  </ListGroup.Item>
+                </ListGroup>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6}>
+              <h2 className="m-3">Reviews</h2>
+
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <strong>John Doe</strong>
+                  <p>04/08/2022</p>
+                  <p>review comment </p>
+                </ListGroup.Item>
+
+                <ListGroup.Item>
+                  <p>
+                    nisl suscipit adipiscing bibendum est ultricies integer quis
+                    auctor elit sed vulputate mi sit amet
+                  </p>
+
+                  <Form>
+                    <Form.Group controlId="rating">
+                      <Form.Label>Rating</Form.Label>
+                      <Form.Control as="select">
+                        <option value="">Select...</option>
+                        <option value="1">1 - Poor</option>
+                        <option value="2">2 - Fair</option>
+                        <option value="3">3 - Good</option>
+                        <option value="4">4 - Very Good</option>
+                        <option value="5">5 - Excellent</option>
+                      </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId="comment">
+                      <Form.Label>Comment</Form.Label>
+                      <Form.Control as="textarea" row="3"></Form.Control>
+                    </Form.Group>
+                    <Button type="submit" variant="dark" className="my-3">
+                      Submit
+                    </Button>
+                  </Form>
                 </ListGroup.Item>
               </ListGroup>
             </Col>
